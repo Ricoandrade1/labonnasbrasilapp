72ed53572b6eaa8162b6ef27ec95ef1c610d5f28
ricoandrade1 <ricoandrade1@users.noreply.github.com>
2025-02-21 02:29:28 +0000

    Removido .tsx da importação do App em main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registrado com sucesso:', registration);
-      })
-      .catch(error => {
-        console.log('Erro ao registrar o Service Worker:', error);
-      });
-  });
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registrado com sucesso:', registration);
+    })
+    .catch(error => {
+      console.log('Erro ao registrar o Service Worker:', error);
+    });
 }
 
 createRoot(document.getElementById("root")!).render(<App />);
